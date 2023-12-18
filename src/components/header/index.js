e494282b00309423903aeacca23a1046b5777702
sudarshan <sudarshan29042000.com>
2023-12-18 18:15:59 +0530

    Adding the Header and Footer

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import img from "./logo.svg";
+import "./header.css";
 
 export default function Header() {
   return (
     <div>
-      <h1>Header</h1>
-      <button type="button" class="btn btn-primary">Primary</button>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand d-flex align-items-center" href="#">
+            <img src={img} alt="Logo" width="300" height="54" className="d-inline-block align-text-top" />
+            <div className="header ms-2 d-flex flex-column align-items-center">
+              <span className="d-block">Course Rater</span>
+            </div>
+          </a>
+        </div>
+      </nav>
     </div>
   );
 }
