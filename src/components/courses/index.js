0ec5287e30b0045310aab1dc3b8a352b1c593259
Dilli Prasad <dilliprasadreddy0000@gmail.com>
2023-12-19 13:07:56 +0530

    addding the stars, changed api, likes and dislikes

--- src/components/courses/index.js
+++ src/components/courses/index.js
@@ -1,12 +1,37 @@
 import React, { useState, useEffect } from "react";
+import RatingStars from './components/ratingStars'; 
 import "./style.css";
 
 export default function Courses() {
   const [coursesData, setCoursesData] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const baseUrl = "http://192.168.1.74:5001";
+  const apiEndpoint = `${baseUrl}/courses`;
+
+  const handleRating = (courseId, userRating) => {
+    const requestOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userRating,
+      }),
+    };
+
+    fetch(`${baseUrl}/courses/${courseId}`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response from backend:", data);
+      })
+      .catch((error) => {
+        console.error("Error sending rating:", error);
+      });
+  };
+
   useEffect(() => {
-    fetch("http://192.168.0.121:5002/all_courses")
+    fetch(apiEndpoint)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -17,7 +42,7 @@ export default function Courses() {
         console.error("Error fetching data:", error);
         setLoading(false);
       });
-  }, []);
+  }, [apiEndpoint]);
 
   return (
     <div>
@@ -28,46 +53,42 @@ export default function Courses() {
         <div className="container container-background">
           <div className="row">
             {coursesData &&
-              coursesData.map(
-                ({ heading, imgSrc, likes, dislikes, description }, idx) => (
-                  <div className="col-lg-4 mb-4" key={idx}>
-                    <div className="card">
-                      <img
-                        src={imgSrc}
-                        className="card-img-top"
-                        alt={heading}
-                        style={{ height: "200px" }}
-                      />
-                      <div className="card-body">
-                        <h5 className="card-title">{heading}</h5>
-                        <p className="card-text">{description}</p>
-                        <p className="card-text">
-                          <i
-                            className="material-icons"
-                            style={{
-                              marginRight: "8px",
-                              verticalAlign: "middle",
-                            }}
-                          >
-                            thumb_up
-                          </i>{" "}
-                          {likes} |
-                          <i
-                            className="material-icons"
-                            style={{
-                              marginLeft: "8px",
-                              verticalAlign: "middle",
-                            }}
-                          >
-                            thumb_down
-                          </i>{" "}
-                          {dislikes}
-                        </p>
-                      </div>
+              coursesData.map(({ id, title, photo, likes, dislikes, total_rating }) => (
+                <div className="col-lg-4 mb-4" key={id}>
+                  <div className="card">
+                    <div className="total-rating">{total_rating}/5</div>
+                    <RatingStars totalRating={total_rating} /> 
+                    <img src={photo} className="card-img-top" alt={title} />
+                    <div className="card-body">
+                      <h5 className="card-title">{title}</h5>
+                      <p className="card-text">
+                        <i
+                          onClick={() => handleRating(id, "like")}
+                          className="material-icons"
+                          style={{
+                            marginRight: "8px",
+                            verticalAlign: "middle",
+                          }}
+                        >
+                          thumb_up
+                        </i>{" "}
+                        {likes} |
+                        <i
+                          onClick={() => handleRating(id, "dislike")}
+                          className="material-icons"
+                          style={{
+                            marginLeft: "8px",
+                            verticalAlign: "middle",
+                          }}
+                        >
+                          thumb_down
+                        </i>{" "}
+                        {dislikes}
+                      </p>
                     </div>
                   </div>
-                )
-              )}
+                </div>
+              ))}
           </div>
         </div>
       )}
